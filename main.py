8f8f8bc56ce0cc454fb450d5aca5f6a0fbd43c5e
Mahdi <mbk-24-0253@mbk-24-0253.mpi-sws.org>
2025-03-29 01:37:36 +0330

    fixes

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from models.car2d import Robot2D
 from source.abstraction import Abstraction
 
-prism_executable='/Users/mbk-24-0253/Desktop/IMDPSYN/prism-4.8.1-mac64-arm/bin/prism'
-foldername='/Users/mbk-24-0253/Desktop/IMDPSYN/output'
+prism_executable='./prism-4.8.1-mac64-arm/bin/prism'
+foldername='./output'
 
-abstraction = Abstraction(Robot2D(), '/Users/mbk-24-0253/Desktop/IMDPSYN/models/car2d.conf')
+abstraction = Abstraction(Robot2D(), './models/car2d.conf')
 
 abstraction.generate_noisy_observations()
 abstraction.find_actions()
